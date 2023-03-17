393b1cb1668abb3bfce104aee0ac0e674d7fb918
yuasalily <yuasalily1011@gmail.com>
2023-03-18 01:45:51 +0900

    Exchange追加

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,6 @@
+## 0.0.3
+* Exchange追加
+
 ## 0.0.2
 * GetForObject追加
 
--- pkg/requests.go
+++ pkg/requests.go
@@ -1,11 +1,38 @@
 package pkg
 
 import (
+	"bytes"
 	"encoding/json"
-	"fmt"
 	"log"
 	"net/http"
 )
+
+func Exchange[T any](url string, method string, body any, headers map[string]string) T {
+	byteBody, err := json.Marshal(body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	req, err := http.NewRequest(method, url, bytes.NewBuffer(byteBody))
+	for k, v := range headers {
+		req.Header.Set(k, v)
+	}
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	client := http.Client{}
+	res, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	var response T
+
+	err = json.NewDecoder(res.Body).Decode(&response)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return response
+}
 
 func GetForObject[T any](url string) T {
 
@@ -25,7 +52,5 @@ func GetForObject[T any](url string) T {
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println("response: ", response)
 	return response
-
 }
